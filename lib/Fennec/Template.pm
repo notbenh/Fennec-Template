b2a43e2517eab36d0680d9c4e2bc2283e4f4986e
Chad Granum <exodist7@gmail.com>
2010-07-15 11:14:56 -0700

    Added plan, no time to code it atm

--- lib/Fennec/Template.pm
+++ lib/Fennec/Template.pm
@@ -7,7 +7,12 @@ add_config 'templates';
 
 add_test_hook( sub {
     my ( $runner, $test ) = @_;
-
+    # Plan:
+    # Iterate all the templates in the above config option, as well as those in the test objects stash.
+    # Add the workflows to the test objects root workflow
+    # * Adding the workflow should set/reset parent
+    # * Next test object this is done for will do the same
+    # * no ithreads so this is safe
 });
 
 1;
